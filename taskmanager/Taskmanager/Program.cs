41d5bdb7638ab9c9cff8308682553a59db44eb4a
Michael Höflinger <michaelhoflinger1@gmail.com>
2024-04-23 12:14:57 +0200

    Tasmanager init

---
+++ taskmanager/Taskmanager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Taskmanager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
